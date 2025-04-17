040412f0be98eb952e68164d1daacf8369f4d988
aurelianomaia <e.scola@hotmail.com>
2025-04-17 20:56:02 -0300

    Criando a classe e construtor  e passando parametros

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Pessoa pessoa = new Pessoa("Maria",123);
+        System.out.println(pessoa.nome + "\n"+ pessoa.telefone);
+
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+String nome;
+int telefone;
+
+public Pessoa(String nome, int telefone){
+    this.nome = nome;
+    this.telefone = telefone;
+}
+}
