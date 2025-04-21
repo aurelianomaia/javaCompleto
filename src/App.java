f3a294abaf9f73e877706356356e1bfbe0874a4a
aurelianomaia <e.scola@hotmail.com>
2025-04-21 19:28:37 -0300

    adicionando botoes em java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
+import javax.swing.JFrame;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Pessoa pessoa = new Pessoa("Maria",123);
         System.out.println(pessoa.nome + "\n"+ pessoa.telefone);
+
+        Botao botao1 = new Botao();
+  
+  botao1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  botao1.setSize(350,80);
+  botao1.setVisible(true);
 
     }
 }
