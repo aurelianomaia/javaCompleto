849472b475a00c6b1e7991c3541ea9f34efad4be
aurelianomaia <e.scola@hotmail.com>
2025-04-22 09:42:28 -0300

    criando botao java

--- src/App.java
+++ src/App.java
@@ -1,17 +1,18 @@
 import javax.swing.JFrame;
 
+import java.awt.GridBagLayout;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Pessoa pessoa = new Pessoa("Maria",123);
-        System.out.println(pessoa.nome + "\n"+ pessoa.telefone);
+        Pessoa pessoa = new Pessoa("Maria", 123);
+        System.out.println(pessoa.nome + "\n" + pessoa.telefone);
 
         Botao botao1 = new Botao();
-  
-  botao1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  botao1.setSize(350,80);
-  botao1.setVisible(true);
 
+        botao1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        botao1.setLayout(new GridBagLayout());
+        botao1.setSize(500, 300);
+        botao1.setVisible(true);
     }
 }
